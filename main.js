a187157803ba793fa3576d900ca982b9a2752361
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-01-30 03:28:21 +0530

    made folder to organize files using nodejs

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let inputArr = process.argv.slice(2);
+let fs = require("fs");
+let path = require("path");
 // console.log(inputArr);
 // node main.js tree "directoryPath"
 // node main.js organize "directoryPath"
 // node main.js help
-let command=inputArr[0];
-switch (command){
+let command = inputArr[0];
+switch (command) {
     case "tree":
         treeFn(inputArr[1]);
         break;
@@ -19,15 +21,38 @@ switch (command){
         break;
 }
 
-function treeFn(dirPath){
+function treeFn(dirPath) {
     console.log("Tree command implemented for ", dirPath);
 }
 
-function organizeFn(dirPath){
-    console.log("Organize command implemented for ", dirPath);
+function organizeFn(dirPath) {
+    // console.log("Organize command implemented for ", dirPath);
+    // 1. input -> directory path given
+    if (dirPath == undefined) {
+        console.log("Kindly enter the path");
+        return;
+    }
+    else {
+        let doesExist = fs.existsSync(dirPath);
+        if (doesExist) {
+            // 2. create -> organized_files -> directory
+            let destPath=path.join(dirPath, "organized_files");
+            if(fs.existsSync(destPath)==false){
+                fs.mkdirSync(destPath);
+            }
+        }
+        else {
+            console.log("Kindly enter the correct path");
+            return;
+        }
+    }
+    // 3. identify categories of all the files present in that input directory ->
+    // 4. copy / cut files to that organized directory inside of any of category folder
+
 }
 
-function helpFn(){
+// help function
+function helpFn() {
     console.log(`
     List of All the commands:
         node main.js tree "directoryPath"
